e831d2731de1dc504752ec154beb66b02c1f0cbc
Md Abu Ahad <117459433+Ahad188@users.noreply.github.com>
2022-11-04 23:10:26 +0600

    Add files via upload

---
+++ rs-4.js
@@ -0,0 +1,16 @@
+     
+     const header = document.querySelector("header");
+     window.addEventListener("scroll",function(){
+          header.classList.toggle("sticky",this.window.scrollY > 0);
+     });
+     let menu = document.querySelector("#menu-icon");
+     let nav = document.querySelector(".nav");
+
+     window.onclick =()=>{
+          menu.classList.toggle("bx-x");
+          nav.classList.toggle("active");
+     };
+     window.onscroll = ()=>{
+          menu.classList.remove("bx-x");
+          nav.classList.remove("active");
+     };
